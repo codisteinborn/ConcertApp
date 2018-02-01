d1b00a37cd6169c39fe3b2c8938a5541316adcba
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-02-01 15:32:01 -0600

    send user to our page once authorized

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -2,7 +2,7 @@ var followArray = [];
 var tokenURL = "";
 
 $("#login").on("click", function(){
-    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://joefitz12.github.io/testing/");
+    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://codisteinborn.github.io/ConcertApp/");
 });
 
 tokenURL = window.location.href;
