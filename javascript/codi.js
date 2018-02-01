ed7bf861d35add37b72e02b471c8074765a631c4
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-02-01 11:57:47 -0600

    city specific search

--- javascript/codi.js
+++ javascript/codi.js
@@ -1,15 +1,24 @@
-var artist = "Taylor Swift";
-var city = "Chicago";
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "&city=" + city + "&apikey=f4oDs35w3TxVEHx3jnVKhKSCH7IW63g7";
+var artist = "Slayer";
+var city = "";
+var queryURL = "";
+var concertInfo = {};
 
-
-$.ajax({
-    url: queryURL,
-    type: "GET",
-    dataType: "json",
-}).then(function (response) {
-    console.log("name", response._embedded.events[0].name);
-    console.log("date", response._embedded.events[0].dates.start.localDate);
-    console.log("name", response._embedded.events[0]._embedded.venues[0].name);
-    console.log("url", response._embedded.events[0].url);
+$(".btn").on("click", function (event) {
+    city = String($("#city").val());
+    queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "&city=" + city + "&radius=30&apikey=f4oDs35w3TxVEHx3jnVKhKSCH7IW63g7";
+    $.ajax({
+        url: queryURL,
+        type: "GET",
+        dataType: "json",
+    }).then(function (response) {
+        concertInfo = {
+            name: response._embedded.events[0].name,
+            date: response._embedded.events[0].dates.start.localDate,
+            venue: response._embedded.events[0]._embedded.venues[0].name,
+            url: response._embedded.events[0].url
+        }
+        console.log(response);
+        console.log(queryURL);
+        console.log(concertInfo)
+    });
 });
