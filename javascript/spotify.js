2f01e93030efad2eb5bc87b3a21bc9434a7cc8e2
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-01 14:42:14 -0600

    adds spotify api access/authorization #3

---
+++ javascript/spotify.js
@@ -0,0 +1,34 @@
+var followArray = [];
+var tokenURL = "";
+
+$("#login").on("click", function(){
+    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://joefitz12.github.io/testing/");
+});
+
+tokenURL = window.location.href;
+
+var URLArray = tokenURL.split("");
+var first = URLArray.indexOf("=") + 1;
+var last = URLArray.indexOf("&");
+var token = tokenURL.substring(first, last);
+
+if (last > 0){
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
+        headers: {
+        'Authorization': 'Bearer ' + token
+        },
+        success: function(response) {
+            console.log(response);
+
+            var followList = function (){
+                for (i = 0; i < response.artists.items.length; i++){
+                artistArray.push(response.artists.items[i].name);
+                }
+            }
+
+            followList();
+            console.log("followArray", followArray);
+        }
+    });
+}
